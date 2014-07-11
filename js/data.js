4b45274e3af5349c1d99a46f77859c9f24229def
antoine <aponsard@cs.ubc.ca>
2014-07-10 20:13:08 -0700

    basic search box functionality

---
+++ js/data.js
@@ -0,0 +1,219 @@
+var colors = {
+	white:"255,255,255",
+	green:"176,229,124",
+	lightblue:"180,216,231",
+	darkblue:"130,201,236",
+	yellow:"255,236,148",
+	pink:"255,174,174"
+};
+
+var lists = [
+{
+	name: "Inbox",
+	color: colors.darkblue,
+	show: true,
+	searchResults: [],
+	newTask: "Add task...",
+	taskCompleted:0,
+	tasks: [
+	{
+		description: "call back Amy",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	},{
+		description: "call back Amy",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	}]
+},
+{
+	name: "Groceries",
+	color: colors.pink,
+	show: true,
+	searchResults: [],
+	newTask: "Add task...",
+	taskCompleted:0,
+	tasks: [
+	{
+		description: "call back Amy",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	},{
+		description: "call back Amy",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	}]
+},
+{
+	name: "Long term",
+	color: colors.yellow,
+	show: true,
+	searchResults: [],
+	newTask: "Add task...",
+	taskCompleted:0,
+	tasks: [
+	{
+		description: "call back Amy",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	},{
+		description: "call back Amy",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	}]
+},
+{
+	name: "Academic",
+	color: colors.green,
+	show: true,
+	searchResults: [],
+	newTask: "Add task...",
+	taskCompleted:0,
+	tasks: [
+	{
+		description: "call back Antoine",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	},{
+		description: "call back Amy",
+		completed: false
+	},{
+		description: "check emails",
+		completed: false
+	},{
+		description: "laundry",
+		completed: false
+	},{
+		description: "7pm restaurant",
+		completed: false
+	},{
+		description: "Tomorrow pay phone bill",
+		completed: false
+	},{
+		description: "Apply for Fair Pharmacare",
+		completed: false
+	},{
+		description: "Kick-Ass 2",
+		completed: false
+	}]
+},
+];
